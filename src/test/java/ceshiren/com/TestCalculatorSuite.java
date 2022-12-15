7e1ca5be82eac23170944540bc4c1ef4e2fd4817
ZhuZhu2022 <961032365@qq.com>
2022-12-15 16:33:23 +0800

    update pom.xml

--- src/test/java/ceshiren/com/TestCalculatorSuite.java
+++ src/test/java/ceshiren/com/TestCalculatorSuite.java
@@ -1,10 +1,24 @@
 package ceshiren.com;
 
+import org.junit.platform.suite.api.IncludePackages;
 import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@SelectClasses({ceshiren.com.TestSum.class})
+/*
+@SelectClasses({
+        ceshiren.com.TestSum.class,
+        ceshiren.com.TestParallel.class,
+        concatStr.com.TestConcatStr.class,
+        test.com.TestSub.class
+})
+ */
+@SelectPackages({
+        "ceshiren.com"
+
+})
+@IncludePackages("ceshiren.com")
 public class TestCalculatorSuite {
 
 }
